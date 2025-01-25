1745851b2c53d27a0242fa96abbf73c1a521d7df
Christopher Okafor <chrisoxygen1234@gmail.com>
2025-01-25 04:28:52 +0100

    add suspense boundries to sign in and sign up page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,9 +1,12 @@
 import AuthForm from "@/components/AuthForm";
+import { Suspense } from "react";
 
 function SignInPage() {
   return (
     <div className="text-white grid h-full place-items-center ">
-      <AuthForm formType="sign in" />
+      <Suspense fallback={<div>Loading...</div>}>
+        <AuthForm formType="sign in" />
+      </Suspense>
     </div>
   );
 }

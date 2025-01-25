f253f2fc3696fdd0cba661a4fcaa5910d6c58528
Christopher Okafor <chrisoxygen1234@gmail.com>
2025-01-25 04:31:02 +0100

    fix responsive issue on password reset link page

--- components/PasswordResetLinkSent.tsx
+++ components/PasswordResetLinkSent.tsx
@@ -7,8 +7,8 @@ function PasswordResetLinkSent() {
         src="https://lottie.host/af0c94e0-bf4a-4f77-ad09-75adc87b95e1/tKFRNgXJFU.lottie"
         autoplay
       />
-      <div className="flex flex-col gap-2 items-center">
-        <h2 className="text-white text-4xl font-thin">
+      <div className="flex flex-col gap-2 items-center text-center">
+        <h2 className="text-white md:text-4xl text-2xl font-thin">
           Password Reset Link Sent
         </h2>
         <p className="text-white/70 font-poppins ">
